b755eabd8474f5161e6a40865cbc11af2673c756
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-23 00:07:20 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,22 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     montague:{
+       patriarch:{
+         name:"Lord Montague",
+         Age:53
+       },
+       matriarch:{
+         name:"Lady Montague",
+         Age:54
+       },
+       hero:{
+         name:"Romeo",
+         age:15,
+         status:"alive"
+       },
+       hero_friends:[
+         
+       ]
+     }
+   }
 end
